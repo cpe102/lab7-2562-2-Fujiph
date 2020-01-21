4b7dd63ee9fda83777ec1da13ff6c034b6511dd2
lab302-37 <lab302-37@labeng.local>
2020-01-21 15:51:18 +0700

    v1

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,31 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main () {
+    string status;
+    int age,money,height;
+    cout << "Input age : ";
+    cin >> age;
+    if ( age <= 20 ) {
+        cout << "Input your height";
+        cin >> height;
+        if ( height < 160 ) { status = "UNFRIEND"; }
+        else if ( height < 175 ) { status = "FRIEND"; }
+        else {
+            cout << "How much money do you have? : ";
+        cin >> money;
+            if ( money > 69000000 ) { status = "MARRIED"; }
+            else { status = "ONE-NIGHT-STAND"; }
+        }
+    }
+    else if ( age > 30 ) {
+        cout << "How much money do you have? : ";
+        cin >> money;
+        if ( money > 10000000 ) { status = "UNFRIEND"; }
+        else {
+            status = "BEST FRIEND";
+        }
+    cout << status;
+    }
